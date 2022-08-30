a93cab056e4fa5e313044b4a079074788e652884
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-08-31 00:03:43 +0600

    Lesson-39: Added CartPage

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar';
 import SideBar from './SideBar';
 import Footer from './Footer';
+import PageHero from "./PageHero"
 
 export {
     Navbar,
     SideBar,
-    Footer
+    Footer,
+    PageHero
 }
