0a94caa4e527a007b921072e8baa17acf8d9698c
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-09-01 01:30:57 +0600

    Lesson-40: Updated Cart Reducer,CheckoutPage

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,14 @@ import Navbar from './Navbar';
 import SideBar from './SideBar';
 import Footer from './Footer';
 import PageHero from "./PageHero"
+import StripeCheckout from './StripeCheckout';
+import CartContent from "./CartContent"
 
 export {
     Navbar,
     SideBar,
     Footer,
-    PageHero
+    PageHero,
+    StripeCheckout,
+    CartContent
 }
