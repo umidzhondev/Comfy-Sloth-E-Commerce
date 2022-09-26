b921d0ce319436daf516adab325ea215436923c4
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-09-27 00:41:34 +0600

    Lesson-45: Added Filter Context and reducers

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ProductsProvider } from "./context/products_context";
-import { UserProvider } from "./context/user_context";
-import { CartProvider } from "./context/cart_context";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals'; 
+import { ProductsProvider } from './context/products_context';
+import { UserProvider } from './context/user_context';
+import { CartProvider } from './context/cart_context';
+import { FilterProvider } from './context/filter_context';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <UserProvider>
-      <ProductsProvider>
-        <CartProvider>
-          <App />
+  <React.StrictMode> 
+    <UserProvider> 
+      <ProductsProvider> 
+        <FilterProvider>
+            <CartProvider>
+           <App />
         </CartProvider>
+        </FilterProvider>
+      
       </ProductsProvider>
     </UserProvider>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
